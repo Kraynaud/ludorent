e990a10de0064a5e46fdc04023a0c83527c60080
kevin <kraynaud@gmail.com>
2020-03-10 18:15:29 +0100

    dashboard in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboard
+    @my_games = current_user.games
+    @my_rentals = current_user.rentals
+  end
 end
