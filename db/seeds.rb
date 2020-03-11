86e58c7fa7f20c865682ab139376b4e2e842ae1d
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-11 12:14:56 +0100

    work suspended

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
 Game.destroy_all
 
 User.destroy_all
 
+file_1 = URI.open('https://avatars3.githubusercontent.com/u/58363552?s=460&v=4')
+user = User.new({first_name: "Kevin", last_name: "Raynaud", email: "myemal@ddedede.com", password: "ilikenobes" })
+user.photo.attach(io: file_1, filename: 'kevin.png', content_type: 'image/png')
+user.save!
 
-user = User.create!({first_name: "Kevin", last_name: "Raynaud", email: "myemal@ddedede.com", password: "ilikenobes"})
-
-Game.create!([{
+file_2 = URI.open('https://www.claires.com/dw/image/v2/BBTK_PRD/on/demandware.static/-/Sites-master-catalog/default/dw1e4374e5/images/hi-res/65079_1.jpg?sw=734&sh=734&sm=fit')
+game_1 = Game.new({
   name: "Ant-Man",
   description: "Armed with the astonishing ability to shrink in scale",
   price_per_day: 5,
   number_players: 4,
-  duration: 30,
+  duration: 30,s
   age_limit: 10,
   user_id: user.id
-},
-{
+})
+game_1.photo.attach(io: file_2, filename: 'uno.png', content_type: 'image/png')
+game_1.save!
+
+game_2 = Game.new({
   name: "Pixels",
   description: "Armed with the astonishing ability to shrink in scale",
   price_per_day: 10,
@@ -30,8 +37,11 @@ Game.create!([{
   duration: 45,
   age_limit: 15,
   user_id: user.id
-},
-{
+})
+game_2.photo.attach(io: file_2, filename: 'uno.png', content_type: 'image/png')
+game_2.save!
+
+game_3 = Game.new({
   name: "Terminator Genisys",
   description: "Armed with the astonishing ability to shrink in scale",
   price_per_day: 7,
@@ -39,6 +49,36 @@ Game.create!([{
   duration: 15,
   age_limit: 5,
   user_id: user.id
-}])
+})
+game_3.photo.attach(io: file_2, filename: 'uno.png', content_type: 'image/png')
+game_3.save!
+
+# Game.new([{
+#   name: "Ant-Man",
+#   description: "Armed with the astonishing ability to shrink in scale",
+#   price_per_day: 5,
+#   number_players: 4,
+#   duration: 30,s
+#   age_limit: 10,
+#   user_id: user.id
+# },
+# {
+#   name: "Pixels",
+#   description: "Armed with the astonishing ability to shrink in scale",
+#   price_per_day: 10,
+#   number_players: 8,
+#   duration: 45,
+#   age_limit: 15,
+#   user_id: user.id
+# },
+# {
+#   name: "Terminator Genisys",
+#   description: "Armed with the astonishing ability to shrink in scale",
+#   price_per_day: 7,
+#   number_players: 2,
+#   duration: 15,
+#   age_limit: 5,
+#   user_id: user.id
+# }])
 
 puts "3 games created"
