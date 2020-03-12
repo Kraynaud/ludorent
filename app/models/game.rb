bcd321146c0970b8a11d9991ed168d74f0444fb8
eve jourdan <eve.jourdan@gmail.com>
2020-03-12 11:14:29 +0100

    add tokenkey

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,5 @@ class Game < ApplicationRecord
   has_many :rentals
   has_one_attached :photo
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :will_save_change_to_address?
 end
