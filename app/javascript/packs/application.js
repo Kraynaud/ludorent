7849f27540c59d80e76c538aca403ede1519f929
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-12 17:32:04 +0100

    JS problem solve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { loadDynamicBannerText } from '../components/banner';
-loadDynamicBannerText();
 
 initMapbox();
+loadDynamicBannerText();
