665b3ab99a29120bd90e024412344e1ceed8f433
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-12 12:54:08 +0100

    validations done

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,8 @@
 class Rental < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
 end
