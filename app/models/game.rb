6919fb0df1a3bb4e82ce66ff3b172c5536881977
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-12 16:53:06 +0100

    dependent drestroy sur game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
