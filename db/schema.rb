ca4632a729e5dc5102ec0181f47a6491678b3d85
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-13 16:07:11 +0100

    button css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_094339) do
+ActiveRecord::Schema.define(version: 2020_03_13_113138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,9 +68,9 @@ ActiveRecord::Schema.define(version: 2020_03_13_094339) do
     t.integer "rating"
     t.text "content"
     t.bigint "game_id"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["game_id"], name: "index_reviews_on_game_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
