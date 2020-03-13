e294f7250da4ae73baadcfc07eb3752c28fb05a6
Bertrand Bichat <bertrand.bichat@gmail.com>
2020-03-13 16:47:41 +0100

    unknown people

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Dynamise tes soirées", "Cherche les jeux les plus prés de chez toi"],
+    strings: ["Dynamise tes soirées", "Cherche les jeux les plus près de chez toi"],
     typeSpeed: 50,
     loop: true
   });
